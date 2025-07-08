cfb79404988f295b30a8197b2c43d6f3122e411f
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2025-07-08 23:46:10 +0300

    Update ESLint configuration, modify Nuxt.js compatibility settings, and switch to nightly Nuxt version. Add app.vue for main application structure.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils'
-  ]
-})
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils"],
+});
